f2b189142532edd35b147b05742eef8d7f8951aa
Yevgen <yevgensm@gmail.com>
2025-02-11 11:55:29 -0600

    робота з селф

--- Ai-GitPython/Practice10.py
+++ Ai-GitPython/Practice10.py
@@ -3,19 +3,34 @@
 #опис класу
 
 class Dog:
-    name = 'Monty'
-    age = 2
-    weight =5
+    #constructor
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age =age
+        if age < 0:
+            raise ValueError('Age must be >0')
+        self.weight = weight
+
+
+    # name = 'Monty'
+    # age = 2
+    # weight =5
 
     #можливість гавкати
 
     def make_sound(self):
         print('Gav')
 
-dog1  = Dog()
+    def print_info(self):
+        print(self.name, self.age, self.weight)
+
+
+dog1  = Dog(name='Lev', age=3, weight=5)
+
 
 print(dog1.name)
-print(dog1.age)
+
+dog1.print_info()
 
 
 
