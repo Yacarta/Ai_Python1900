53b985cd265313c14f6025ba48bb0fa966cb4574
Yevgen <yevgensm@gmail.com>
2025-02-11 13:17:41 -0600

    Task4

--- Ai-GitPython/Practice10.py
+++ Ai-GitPython/Practice10.py
@@ -62,25 +62,67 @@ class Student:
 # Створіть список з 3-ма студентами, дані вводить
 # користувач. Після чого для кожного студента виведіть
 # інформацію про нього за допомогою метода.
-
-students = []
-for i in range(3):
-    name = input('Імя')
-    age = int(input('Age'))
-
-    student = Student(name, age)
-    students.append(student)
-
-for student in students:
-    student.print_info()
+#
+# students = []
+# for i in range(3):
+#     name = input('Імя')
+#     age = int(input('Age'))
+#
+#     student = Student(name, age)
+#     students.append(student)
+#
+# for student in students:
+#     student.print_info()
 # Завдання 3
 # Створіть клас Circle з атрибутом radius. Додайте метод для
 # отримання площі кола
+
+# class Circle:
+#     def __init__(self, radius):
+#         self.radius = radius
+#
+#     def get_area(self):
+#         return 3.14 * self.radius ** 2
+#
+#     circle1 = Circle(2)
+#
+#     area1 = circle1.get_area()
+#
+#     print(area1)
 # Завдання 4
 # Створіть клас BankAccount з атрибутами owner та balance.
 # Додайте метод deposit для поповнення рахунку
 # Додайте метод withdraw для зняття грошей з рахунку
 # Додайте метод info для виведення інформації про баланс
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Недостатньо коштів")
+
+
+    def print_info(self):
+        print(f'Власник: {self.owner} на рахунку {self.balance}')
+
+
+acount = BankAccount('Tom', 2000)
+acount.print_info()
+acount.deposit(500)
+acount.print_info()
+acount.withdraw(3000)
+acount.print_info()
+
+
+
 # Завдання 5
 # Створіть клас Car з атрибутами brand(марка), year(рік
 # випуску), is_ready(чи готовий до поїздки, за замовчування
