b005dcf7633e0759575ef4fac2f8d4cd54e858d6
Yevgen <yevgensm@gmail.com>
2025-02-13 11:28:30 -0600

    Create class project

--- Ai-GitPython/main.py
+++ Ai-GitPython/main.py
@@ -1,4 +1,4 @@
-#  Завдання 1
+# Завдання 1
 # Створіть клас Проект з атрибутами:
 #  назва
 #  виділений кошторис
@@ -16,15 +16,36 @@
 # виконання
 #  поповнення кошторису
 
-class Project:
 
-    def __init__(self, name, budget,
-                 expenses, tasks):
+class Project:
+    def __init__(self, name, budget, tasks):
         self.name = name
         self.budget = budget
-        self.expenses = expenses
         self.tasks = tasks
-        self.is_fibished = False
-        self.spent_time = 0
+
+        self.expenses = 0
+        self.is_finished = False
+        self.spent_time = 0  # місяці
+
+    def display_info(self):
+        print(f"Проєкт {self.name}")
+        print(f'Час виконання {self.spent_time} місяців')
+        print(f'Витрачено {self.expenses}/{self.budget}$')
+        print(f'Залишилось {self.budget - self.expenses}$')
+
+        if self.is_finished: # якщо проект завершений
+            print("Стан проекту: завершений")
+        else:
+            print("Стан проекту: незавершений")
+
+            print('Задачі що залишились:')
+            for task in self.tasks:
+                print(f'   - {task}')
 
 
+project = Project(name='Ігрушка',
+                  budget=10_000,
+                  tasks=['Знайти інвесторів',
+                         'Придумати загальну ідею'])
+
+project.display_info()
