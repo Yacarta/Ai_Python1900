747297bd727f248ecfd1148434970a0101682cb5
Yevgen <yevgensm@gmail.com>
2025-02-13 13:29:58 -0600

    add phone

--- Ai-GitPython/main.py
+++ Ai-GitPython/main.py
@@ -83,38 +83,130 @@ class Project:
 
 
 
-project = Project(name='Ігрушка',
-                  budget=10_000,
-                  tasks=['Знайти інвесторів',
-                         'Придумати загальну ідею'])
+# project = Project(name='Ігрушка',
+#                   budget=10_000,
+#                   tasks=['Знайти інвесторів',
+#                          'Придумати загальну ідею'])
+#
+# project.display_info()
+#
+# project.add_task('Вибрати ПЗ для гри')
+#
+# project.display_info()
+#
+# project.divide_task('Організувати бенкет',
+#                     ['витратити бюджет']
+#                     )
+#
+# project.divide_task('Придумати загальну ідею',
+#                     ['Обрати між 2D та 3D',
+#                      'Придумати сюжет',
+#                      'Прописати персонажів']
+#                     )
+#
+# project.display_info()
+#
+# project.do_task('Придумати сюжет', 6, 1000)
+# project.do_task('Обрати між 2D та 3D', 3, 200)
+# project.do_task('Прописати персонажів', 15, 6000)
+#
+# project.update_budget(2500)
+#
+# project.do_task('Знайти інвесторів', 10, 3000)
+# project.do_task('Вибрати ПЗ для гри', 5, 1800)
+#
+# project.display_info()
 
-project.display_info()
+# Створіть клас Телефон з атрибутами:
+#  максимальний обсяг пам’яті #  зайнята пам’ять #  чи включений(за замовчуванням False)
+#  встановлені додатки у вигляді словника, де ключ –# назва додатку, значення – обсяг пам’яті
+# Додайте методи:
+#  вивести інформацію про використання пам’яті
+#  видалити додаток
+#  встановити новий додаток, якщо пам’яті достатньо
+#  оновити додаток(нова версія може займати іншу
+# кількість пам’яті)
+#  запустити додаток, якщо він є і якщо телефон
+# вкючений
+#  включити телефон
+#  виключити телефон
 
-project.add_task('Вибрати ПЗ для гри')
+class Phone:
+    def __init__(self, max_memory, apps):
+        self.max_memory = max_memory
+        self.is_on = False
+        self.apps = apps
 
-project.display_info()
+        self.used_memory = 0 # рахуємо память
+        for app_name in self.apps:
+            self.used_memory += self.apps[app_name]
 
-project.divide_task('Організувати бенкет',
-                    ['витратити бюджет']
-                    )
+    def display_info(self):
+        print(f'використана память {self.used_memory}/{self.max_memory}ГБ')
+        print(f'встановлені додатки')
+        for app_name in self.apps:
+            print(f'встановлений додаток {app_name} -{self.apps [app_name]} ГБ')
+        print()
+        print()
 
-project.divide_task('Придумати загальну ідею',
-                    ['Обрати між 2D та 3D',
-                     'Придумати сюжет',
-                     'Прописати персонажів']
-                    )
+    def delete_app(self, app_name):
+        if app_name not in self.apps:
+            print('немає такого додатку')
+            return
 
-project.display_info()
+        app_memory = self.apps.pop(app_name)
+        self.used_memory -= app_memory
 
-project.do_task('Придумати сюжет', 6, 1000)
-project.do_task('Обрати між 2D та 3D', 3, 200)
-project.do_task('Прописати персонажів', 15, 6000)
+    def turn_on(self):
+        self.is_on = True
 
-project.update_budget(2500)
+    def turn_off(self):
+        self.is_on = False
 
-project.do_task('Знайти інвесторів', 10, 3000)
-project.do_task('Вибрати ПЗ для гри', 5, 1800)
+    def download_app(self, app_name, app_memory):
+        free_memory = self.max_memory - self.used_memory
 
-project.display_info()
+        if free_memory >= app_memory:
+            self.apps[app_name] = app_memory
+            self.used_memory += app_memory
+        else:
+            print('недостатньо памяті')
+
+    def update_app(self, app_name, new_memory):
+        if app_name not in self.apps:
+            print('немає такого додатку!')
+            return
+
+        free_memory = self.max_memory - self.used_memory
+        app_memory = self.apps[app_name]
+        needed_memory = new_memory - app_memory
+
+        if free_memory >= needed_memory:
+            self.apps[app_name] = new_memory
+            self.used_memory += needed_memory
+
+        else:
+            print('недостатньо памяті')
+
+
+
+phone = Phone(128,
+              {'Google': 30,
+              'Ytube': 20,
+               'Telegram':5})
+
+phone.display_info()
+
+phone.delete_app('Ytube')
+
+phone.display_info()
+
+phone.download_app('Angry_birds',20)
+
+phone.display_info()
+
+phone.update_app('angry_birds', 30)
+
+phone.display_info()
 
 
