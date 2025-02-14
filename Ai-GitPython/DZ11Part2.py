ecd2856e1b292373fe4ee87a50be6b150209fdfb
Yevgen <yevgensm@gmail.com>
2025-02-14 16:37:54 -0600

    Модуль 11. ООП частина 1 DZ 1

--- Ai-GitPython/DZ11Part2.py
+++ Ai-GitPython/DZ11Part2.py
@@ -15,46 +15,46 @@
 
 class Bank:
     exchange_rates = {"usd": 41, "eur": 43, "grn": 1}
-
     currency_ukr = {"usd": "Доларів", "eur": "Євро", "grn": "Гривень"}
 
     def __init__(self, name, balans, currency):
         self.name = name
         self.balans = balans
         self.currency = currency
-
         self.is_currency()
 
     def is_currency(self):
-        exchange_rates = {"usd": 41, "eur": 43, "grn": 1}
-        if self.currency not in exchange_rates:
-            raise ValueError(f"Невідома валюта{self.currency}")
+        if self.currency not in self.exchange_rates:
+            raise ValueError(f"Невідома валюта {self.currency}")
 
     def print_info(self):
-        currency_ukr = {"usd": "Доларів", "eur": "Євро", "grn": "Гривень"}
-        print(f"Шановний {self.name}, у Вас на рахунку {self.balans} {currency_ukr[self.currency]}")
+        print(f"Шановний {self.name}, у Вас на рахунку {self.balans} {self.currency_ukr[self.currency]}")
 
-#  перевести гроші з однієї валюти в іншу(ця операція
-# часто використовується
-    def exchenge(self, amount,from_currency, to_currency):
-        # self.is_currency(to_currency)
-        # self.is_currency(from_currency)
+    def exchange(self, amount, from_currency, to_currency):
+        if from_currency not in self.exchange_rates or to_currency not in self.exchange_rates:
+            raise ValueError("Невідома валюта")
 
-        exchange_amount = amount * self.exchange_rates[from_currency]/self.exchange_rates[to_currency]
-        print(f"{amount} {self.currency_ukr[from_currency] } по курсу"
-              f"  = {exchange_amount} {self.currency_ukr[to_currency]} ")
-
+        exchange_amount = amount * self.exchange_rates[from_currency] / self.exchange_rates[to_currency]
+        print(
+            f"{amount} {self.currency_ukr[from_currency]} по курсу = {exchange_amount:.2f} {self.currency_ukr[to_currency]}")
+        return exchange_amount
 
     #  зміна валюти
-    #  поповнення балансу(валюта та сама)
-    #  зняття грошей з балансу(валюта та сама).
     def change_currency(self, currency):
-        self.is_currency(currency)
+        if currency not in self.exchange_rates:
+            raise ValueError("Невідома валюта")
         if currency != self.currency:
-            self.balans = self.
+            self.balans = self.exchange(self.balans, self.currency, currency)
+            self.currency = currency
+            print(
+                f"Ваш рахунок переведено в {self.currency_ukr[currency]}. У Вас {self.balans:.2f} {self.currency_ukr[currency]}")
+# поповнення балансу(валюта та сама)
 
+
+# зняття грошей з балансу(валюта та сама).
 
 
 client = Bank("Jhon", 2000, "usd")
 client.print_info()
-client.exchenge(500, 'usd', 'grn')
+client.exchange(500, 'usd', 'grn')
+client.change_currency("grn")
