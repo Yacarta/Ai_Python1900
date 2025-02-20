d9025a688ad7f95abb1236027ec23721fec2cb21
Yevgen <yevgensm@gmail.com>
2025-02-20 14:33:43 -0600

    create DZ

--- Ai-GitPython/DZ.py
+++ Ai-GitPython/DZ.py
@@ -1,68 +1,65 @@
-#  Завдання 1
-# Створіть клас Cart(кошик клієнта магазину) з атрибутами
-# client(ім’я клієнта) та items(список товарів).
-# Додайте метод який додає новий товар до кошика
-# Додайте метод який видаляє товар з кошика
-# Додайте метод для виведення інформації про кошик
+# Завдання 1
+# Створіть наступні класи:
+#  CreditCardPayment – атрибути currency
+#  PayPalPayment – атрибути currency
+#  CryptoPayment – атрибути currency
 
-class Cart:
-    def __init__(self, client, items):
-        self.client = client
-        self.items = items
+# # Методи:
+# #  pay(amount) – виводить повідомлення
+# # o CreditCardPayment – оплата карткою {amount}{currency}
+# # o PayPalPayment – оплата PayPal {amount}{currency}
+# # o CryptoPayment – оплата криптогаманцем {amount}{currency}
+#
+# # Напишіть функцію create_payment() яка запитує у  користувача тип рахунку та потрібні атрибути і повертає # об’єкт.
+# # Створіть декілька рахунків, добавте їх у список та для  кожної викличте відповідні методи.
+#
+#
+# class CreditCardPayment:
+#     def __init__(self, currency):
+#         self.currency = currency
+#
+#     @staticmethod
+#     def pay( amount):
+#         print(f"Оплата CreditCard {amount} {self.currency} ")
+#
+#
+#
+# class PayPalPayment:
+#     def __init__(self, currency):
+#         self.currency = currency
+#
+#     @staticmethod
+#     def pay(amount):
+#         print(f"Оплата PayPal {amount} {self.currency} ")
+#
+#
+# class CryptoPayment
+#     def __init__(self, currency):
+#         self.currency = currency
+#
+#     @staticmethod
+#     def pay(amount):
+#         print(f"Оплата Crypto {amount} {self.currency} ")
+#
+# # Напишіть функцію create_payment() яка запитує у  користувача тип рахунку та потрібні атрибути і повертає # об’єкт.
+# # Створіть декілька рахунків, добавте їх у список та для  кожної викличте відповідні методи.
+# def create_payment():
+#     print("Types of accounts: CreditCard, PayPal, Crypto" )
+#     type_acount = input("Enter type of account: ")
+#     currency = input("Enter currency")
 
 
-    def add_product(self, product):
-        self.items.append(product)
-        print(f'Товар "{product}" додано до кошика.')
+    if type_acount == "CreditCard" :
+        return CreditCardPayment
+    elif type_acount == "PayPal":
+        return PayPalPayment
+    elif type_acount == "Crypto":
+        return CryptoPayment
 
+payments =[]
+for i in range(3):
+    payment = create_payment()
+    if payment:
+        payments.append(payment)
 
-
-    def delete_product(self, product1):
-        if product1 in self.items:
-            self.items.remove(product1)
-            print(f'Товар "{product1}" видалено з кошика.')
-        else:
-            print(f'Товар відсутній у кошику.')
-
-
-    def print_info(self):
-        print(f"Шановний {self.client} у вас в кошику: {self.items}")
-
-
-
-# shop1 = Cart("Іван", ["Яблуко", "Черешня"])
-# shop1.print_info()
-# shop1.add_product("Груша")
-# shop1.print_info()
-
-
-# Завдання 2
-# Створіть клас Phone з атрибутами number та battery_level.
-# Додайте метод який зменшує заряд телефона(на скільки
-# зменшити відсотків передається як параметр), якщо він
-# опуститься нижче 20%, вивести повідомлення
-# Додайте метод для виведення інформації про телефон.
-class Phone:
-    def __init__(self, number,batery_level):
-        self.number = number
-        self.batery_level = batery_level
-
-    def discharge(self, level):
-
-
-        if self.batery_level < level:
-            print(f"Неможливо зменшити на {level}%. ")
-        else:
-            self.batery_level -= level
-            print(f"Заряд зменшено на {level}%")
-
-    def print_info(self):
-        print(f"Номер телефону {self.number} має {self.batery_level}% зарядкию")
-        if self.batery_level <= 20 and self.batery_level >= 0:
-            print(" Ваш телефон потрібно зарядити")
-
-telephone = Phone(38050320, 80)
-telephone.print_info()
-
-telephone.discharge(65)
-telephone.print_info()
+for payment in payments:
