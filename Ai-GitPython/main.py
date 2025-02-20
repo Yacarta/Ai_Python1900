f4b4544be8f47580cded183b422ecf106cf3ac4b
Yevgen <yevgensm@gmail.com>
2025-02-20 14:31:15 -0600

    create Cart

--- Ai-GitPython/main.py
+++ Ai-GitPython/main.py
@@ -103,7 +103,7 @@ cart2 = Cart(['phone'], 65)
 
 cart3 = Cart(['laptop', 'TV'], 170)
 
-print(cart1 == cart3)
+# print(cart1 == cart3)
 
 #
 # print(cart1)
@@ -156,3 +156,128 @@ print(cart1 == cart3)
 #
 # for item in cart3:  # iter(cart3)
 #     print(item)
+
+# Завдання 1
+# Створіть клас Message з атрибутами
+#  user – ім’я автора повідомлення
+#  text – текст повідомлення
+#  time – час повідомлення(використайте модуль datetime)
+# приклад datetime.strptime('10:23', '%H:%M')
+# методи:
+#  __str__(self) – повертає текст повідомлення та час
+#  __len__(self)  – повертає довжину повідомлення
+#  __gt__(self, other)  – перевіряє чи є повідомлення self
+# старішим за other
+# Створіть список з декількома повідомленнями та виведіть
+# його. Відсортуйте список і знову виведіть
+from  datetime import datetime
+class Message:
+    def __init__(self, user, text, str_time):
+        self.user = user
+        self.text = text
+        self.time = datetime.strptime(str_time, '%H:%M')
+
+    def __str__(self):
+        str_time = datetime.strftime(self.time, '%H:%M')
+        return f"{str_time}--{self.text}--{self.user}"
+
+    def __len__(self):
+        return len(self.text)
+
+    def __gt__(self, other):
+        return self.time > other.time
+
+
+
+
+message1 = Message("Bob", "Hello", "20:00")
+message2 = Message("Marry", "How are you", "20:01")
+message3 = Message("Bob", "Hello", "20:00")
+# print(message1)
+# print(len(message1))
+# print(message1 > message3)
+#
+# messages = [message3, message1, message2]
+# for mess in messages:
+#     print(mess)
+# messages.sort()
+# for mess in messages:
+#     print(mess)
+
+
+# Завдання 2
+# Створіть клас Song з атрибутами
+#  name – назва пісні
+#  author – ім’я автора
+# методи:
+#  __eq__(self, other) – перевіряє чи дві пісні однакові
+#  __str__(self, other) – повертає рядок з назвою та автором
+# Створіть клас Playlist з атрибутами
+#  songs – список пісень(об’єкти класу Song)
+# методи:
+#   __len__(self)  – повертає кількість пісень
+#  __contains__(self, item) – перевіряє чи є пісня в плейлисті
+#  __iter__(self) – повертає літератор для циклу for
+#  add_song(self, song) – додає пісню в плейлист
+#  remove_song(self, song) – видаляє пісню з плейлиста
+# Створіть порожній плейлист
+# Створіть 3 пісні:
+# "Imagine", "John Lennon"
+# "Bohemian Rhapsody", "Queen"
+# "Shape of You", "Ed Sheeran"
+# Добавте їх в плейлист
+# Пройдіться циклом for по плейлисту та виведіть кожну
+# пісню на екран
+
+class Song:
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+
+    def __eq__(self, other):
+        if isinstance(other, Song):
+            return (self.author, self.name) == (other.author, other.name)
+        else:
+            return False
+
+    def __str__(self):
+        return f"{self.author} пісня '{self.name}'"
+
+
+class Playlist:
+    def __init__(self, songs):
+        self.songs = songs
+
+    def __len__(self):
+        return len(self.songs)
+
+    def __contains__(self, song):
+        return song in self.songs
+
+    def __iter__(self):
+        for song in self.songs:
+            yield song
+
+    def add_song(self, song):
+        self.songs.append(song)
+
+    def remove_song(self, song):
+        self.songs.remove(song)
+
+song1 = Song("Imagine", "John Lennon")
+song2 = Song("Bohemian Rhapsody", "Queen")
+song3 = Song("Shape of You", "Ed Sheeran")
+song4 = Song("Imagine", "John Lennon")
+
+playlist = Playlist([])
+playlist.add_song(song1)
+playlist.add_song(song2)
+playlist.add_song(song3)
+print(len(playlist))
+print(song4 in playlist)
+playlist.remove_song(song4)
+
+for song in playlist:
+    print(song)
+
+    print(song)
