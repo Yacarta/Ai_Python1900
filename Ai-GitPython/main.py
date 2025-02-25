e395d40d95c8844f2f3f2f8aa2ee5a5fadab93a4
Yevgen <yevgensm@gmail.com>
2025-02-25 12:10:12 -0600

    car

--- Ai-GitPython/main.py
+++ Ai-GitPython/main.py
@@ -42,3 +42,94 @@
 # # код з класом Parent
 # mother = Parent('Marry', 36)
 # mother.show_info()
+
+
+
+# класи для Транспортні засоби
+
+class Vehicle:
+    def __init__(self, owner,  # власник
+                 max_fuel_level,  # максимальний рівень пального
+                 milliage  # км / 1 літр пального
+                 ):
+        self.owner = owner
+        self.max_fuel_level = max_fuel_level
+        self.fuel_level = max_fuel_level # бак повний
+        self.milliage = milliage
+
+    def move(self, speed, distance):
+        need_fuel = distance / self.milliage
+
+        if self.fuel_level < need_fuel: # не хватає пального
+            print('не хватає пального')
+        else:
+            self.fuel_level -= need_fuel
+            time = distance / speed
+
+            print(f"Проїхали {distance}км за {time}год")
+
+
+    def add_fuel(self, fuel):
+        self.fuel_level += fuel
+
+class Car(Vehicle):
+    pass
+
+
+class Bicycle(Vehicle):
+    def __init__(self, owner, max_fuel_level, milliage):
+        self.owner = owner
+        self.max_fuel_level = max_fuel_level
+        self.fuel_level = max_fuel_level  # бак повний
+        self.milliage = milliage
+
+        self.used_motor = False # мотор виключений
+
+
+    def move(self, speed, distance):  # теж рухається але не трабе пального
+        if not self.used_motor:
+            time = distance / speed
+            print("Без пального")
+            print(f"Проїхали {distance}км за {time}год")
+            return
+
+        # мотор вклюсений(код з Vehicle.move)
+
+        need_fuel = distance / self.milliage
+
+        if self.fuel_level < need_fuel:  # не хватає пального
+            print('не хватає пального')
+        else:
+            self.fuel_level -= need_fuel
+            time = distance / speed
+
+            print("З пальним")
+            print(f"Проїхали {distance}км за {time}год")
+
+    def turn_on(self):
+        self.used_motor = True
+
+    def turn_off(self):
+        self.used_motor = False
+
+
+class Plane(Vehicle):
+    pass
+
+
+
+bike = Bicycle('John', 100, 50)
+bike.move(20, # швидкість
+         15  # кілометри
+         )
+
+bike.turn_on()
+
+bike.move(40,
+          100)
+
+
+
+
+car = Vehicle('John', 100, 50 )
+car.move(80,200)
