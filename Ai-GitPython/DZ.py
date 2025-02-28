9947b6ae1464ba8cd724b467a598527da13c2f97
Yevgen <yevgensm@gmail.com>
2025-02-27 18:41:31 -0600

    dz11-6

--- Ai-GitPython/DZ.py
+++ Ai-GitPython/DZ.py
@@ -1,72 +1,20 @@
-# Завдання 1
-# Створіть наступні класи:
-#  CreditCardPayment – атрибути currency
-#  PayPalPayment – атрибути currency
-#  CryptoPayment – атрибути currency
-
-# # Методи:
-# #  pay(amount) – виводить повідомлення
-# # o CreditCardPayment – оплата карткою {amount}{currency}
-# # o PayPalPayment – оплата PayPal {amount}{currency}
-# # o CryptoPayment – оплата криптогаманцем {amount}{currency}
-#
-# # Напишіть функцію create_payment() яка запитує у  користувача тип рахунку та потрібні атрибути і повертає # об’єкт.
-# # Створіть декілька рахунків, добавте їх у список та для  кожної викличте відповідні методи.
-#
-#
-class CreditCardPayment:
-    def __init__(self, currency):
-        self.currency = currency
-
-    def pay(self, amount):
-        print(f"Оплата CreditCard {amount} {self.currency} ")
-
-
-class PayPalPayment:
-    def __init__(self, currency):
-        self.currency = currency
-
-    def pay(self, amount):
-        print(f"Оплата PayPal {amount} {self.currency} ")
-
-
-class CryptoPayment:
-    def __init__(self, currency):
-        self.currency = currency
-
-    def pay(self, amount):
-        print(f"Оплата Crypto {amount} {self.currency} ")
-
-# Напишіть функцію create_payment() яка запитує у  користувача тип рахунку та потрібні атрибути і повертає # об’єкт.
-# Створіть декілька рахунків, добавте їх у список та для  кожної викличте відповідні методи.
-def create_payment():
-    print("Типи оплати: CreditCard, PayPal, Crypto")
-    type_account = input("Введіть тип оплати: ")
-    currency = input("Введіть тип валюти: ")
-
-    if type_account == "CreditCard":
-        return CreditCardPayment(currency)
-    elif type_account == "PayPal":
-        return PayPalPayment(currency)
-    elif type_account == "Crypto":
-        return CryptoPayment(currency)
-    else:
-        print("Неправильний тип оплати!")
-        return None
-
-
-
-payments = []
-for i in range(3):
-    payment = create_payment()
-    if payment:
-        payments.append(payment)
-
-
-for payment in payments:
-    amount = float(input(f"Введіть сумму: "))
-    # amount = float(input(f"Введіть сумму для оплати з {payment}: "))
-    payment.pay(amount)
-
-
-client = create_payment()
+#  Завдання 1
+# Створіть клас Passenger з атрибутами
+#  name – ім’я
+#  destination – місце, куди прямує
+# Завдання 2
+# Створіть клас Transport з атрибутами
+#  speed – швидкість
+# Методи
+#  move(destination, distance) – рухається до місця
+# призначення, виводить інформацію як довго їхали
+# Завдання 3
+# Створіть клас Bus з атрибутами
+#  passengers – список пасажирів(об’єкти класу Passenger)
+#  capacity – максимальна можлива кількість пасажирів
+# Методи
+#  board_passenger(passenger) – якщо є місце, додає
+# пасажира
+#  move(destination, distance) – висаджує всіх пасажирів, які
+# хочуть вийти в даному місці(виводить їхню загальну
+# кількість) та викликає батьківський метод move()
