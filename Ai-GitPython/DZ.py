162415b0a2dc9e6d4543cced300f23a6268ab3e3
Yevgen <yevgensm@gmail.com>
2025-03-05 15:02:09 -0600

    dz11

--- Ai-GitPython/DZ.py
+++ Ai-GitPython/DZ.py
@@ -18,26 +18,70 @@
 #  move(destination, distance) – висаджує всіх пасажирів, які
 # хочуть вийти в даному місці(виводить їхню загальну
 # кількість) та викликає батьківський метод move()
-from abc import ABC
+from abc import ABC, abstractmethod
 
-class Passenger(ABC):
+class Passenger():
     def __init__(self, name, destination):
         self.name = name
         self.destination = destination
 
-class Transport():
+    def __str__(self):
+        return self.name
+
+class Transport(ABC):
+    @abstractmethod
     def __init__(self, speed):
         self.speed = speed
 
     def move(self, destination, distance):
-        pass
+        time_in_bus = distance / self.speed
+        print('Time in travel to ', destination,' =', time_in_bus)
 
-class Bus():
-    def __init__(self, passengers, capacity):
-        pass
+class Bus(Transport):
+    def __init__(self,speed, capacity):
+        super().__init__(speed)
+        self.passengers = []
+        self.capacity = capacity
 
     def board_passenger(self, passenger):
-        pass
+        if len(self.passengers) >= self.capacity:
+            print('Bus is full')
+        else:
+            self.passengers.append(passenger)
+            print(passenger, ' is in bus')
 
     def move(self, destination, distance):
-        pass
+        super().move(destination, distance)
+        leaving_pass = []
+        left_pass = []
+        for p in self.passengers:
+            if p.destination == destination:
+                leaving_pass.append(p)
+
+            else:
+                left_pass.append(p)
+
+        time_in_bus = distance / self.speed
+
+        print(len(leaving_pass), 'passengers leave the bus after ', time_in_bus, ' Hours')
+        self.passengers = left_pass
+
+
+
+bus1 = Bus(60, 10)
+bus1.move('Lviv', 600)
+pas1 = Passenger('Ivan', 'Dnipro')
+pas2 = Passenger('Yana', 'Lviv')
+
+bus1.board_passenger(pas1)
+bus1.board_passenger(pas2)
+bus1.move('Lviv', 600)
+
+Time in travel to  Lviv  = 10.0
+0 passengers leave the bus after  10.0  Hours
+Ivan  is in bus
+Yana  is in bus
+Time in travel to  Lviv  = 10.0
+1 passengers leave the bus after  10.0  Hours
+
+Process finished with exit code 0
